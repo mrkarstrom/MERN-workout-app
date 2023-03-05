b89cf309f2bcd3346a8e2e50970b659ef337d822
Magnus Karström <magkar@BJURHOLMS.LOCAL>
2023-03-05 14:28:10 +0100

    feat (models/Workout.js): Adding a Schema

--- backend/server.js
+++ backend/server.js
@@ -19,11 +19,11 @@ app.use('/api/workouts', workoutRoutes);
 // Connect to db
 mongoose
   .connect(process.env.MONGO_URI)
-  .then(() => {})
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log('Connected to db, listening on port: ', process.env.PORT);
+    });
+  })
   .catch((error) => {
     console.log(error);
   });
-
-app.listen(process.env.PORT, () => {
-  console.log('Listening on port secret port');
-});
