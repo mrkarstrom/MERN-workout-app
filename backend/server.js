d5c19e10acc54064229160df0f018478f8dff9fb
Magnus Karström <magkar@BJURHOLMS.LOCAL>
2023-03-05 13:33:36 +0100

    feat (.env + server.js): Added the mongoose package and database env-path

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const workoutRoutes = require('./routes/workouts');
 
 const app = express();
+
+// Middleware
+app.use(express.json());
 
 app.use((req, res, next) => {
   console.log(req.path, req.method);
@@ -11,6 +15,14 @@ app.use((req, res, next) => {
 
 // Routes to use
 app.use('/api/workouts', workoutRoutes);
+
+// Connect to db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {})
+  .catch((error) => {
+    console.log(error);
+  });
 
 app.listen(process.env.PORT, () => {
   console.log('Listening on port secret port');
